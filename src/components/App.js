f045a0d624aecb243839db83ecf6f113917e78b2
Emma Perlman <emmajperlman@gmail.com>
2022-05-19 11:30:41 -0700

    Create basic navigation, add Home and About components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar/>
+        <Header/>
+        <Switch>
+          <Route path='/leagueplayers'>
+            <TeamsPlayersPage/>
+          </Route>
+          <Route path='/choosemyteam'>
+            <ChooseMyTeamPage/>
+          </Route>
+          <Route path='/blog'>
+            <BlogPage/>
+          </Route>
+          <Route path='/about'>
+            <About/>
+          </Route>
+          <Route path='/'>
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
