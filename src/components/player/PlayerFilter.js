1ffd1678667f73478ac64f54724aef2be4e8891b
Emma Perlman <emmajperlman@gmail.com>
2022-05-22 15:47:47 -0700

    filter all players on choose my team with search bar

--- src/components/player/PlayerFilter.js
+++ src/components/player/PlayerFilter.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export const PlayerFilter = () => {
+export const PlayerFilter = ({search,onSearchChange}) => {
   return (
-    <div>PlayerFilter</div>
+    <div className="Filter">
+      <input type="text" name="search" value={search} onChange={onSearchChange} placeholder="Search..." />
+      {/* <select name="filter" onChange={onCategoryChange}>
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select> */}
+    </div>
   )
 }
--- src/containers/ChooseMyTeamPage.js
+++ src/containers/ChooseMyTeamPage.js
@@ -1,14 +1,26 @@
 import React,{useState} from "react"
+import { PlayerFilter } from "../components/player/PlayerFilter"
 import {PlayerList} from '../components/player/PlayerList'
 export const ChooseMyTeamPage = ({players}) => {
   const [myPlayers,setMyPlayers]=useState([])
+  const [search,setSearch]=useState('')
+
   function addPlayerMyTeam(addedPlayer){
     setMyPlayers([...myPlayers,addedPlayer])
   }
+
+  function handleSearchChange(event){
+    setSearch(event.target.value)
+  }
+  const shownPlayers=players
+  .filter(player=>player.strPlayer.includes(search)) 
   return (
-    <div style={{display:'flex'}}>
-      <PlayerList players={players} title={'All Players'} onPlayerClick={addPlayerMyTeam}/>
-      <PlayerList players={myPlayers} title={'My Team'} />
+    <div>
+      <PlayerFilter search={search} onSearchChange={handleSearchChange}/>
+      <div style={{display:'flex'}}>
+        <PlayerList players={shownPlayers} title={'All Players'} onPlayerClick={addPlayerMyTeam}/>
+        <PlayerList players={myPlayers} title={'My Team'} />
+      </div>
     </div>
   )
 }
