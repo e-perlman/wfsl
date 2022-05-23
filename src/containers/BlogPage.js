85c5d945976b3ae09d68ebd57f925d38f56eca2f
Emma Perlman <emmajperlman@gmail.com>
2022-05-22 20:21:00 -0700

    fetch upcoming games from sports db

--- src/containers/BlogPage.js
+++ src/containers/BlogPage.js
@@ -1,6 +1,20 @@
 import React from "react"
 export const BlogPage = () => {
+  const fetchGames=async ()=>{
+    try{
+      const resp= await fetch('https://www.thesportsdb.com/api/v1/json/50130162/eventsnextleague.php?id=4521')
+      const data= await resp.json()
+      setTeams(data.events)
+    }catch(error){
+      alert(error)
+    }
+  }
+  useEffect(()=>{
+    fetchGames()
+  },[]);
   return (
-    <div>BlogPage</div>
+    <div>
+      
+    </div>
   )
 }
