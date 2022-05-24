03fa863a35f7b1e31b87f5b83ae720cfdffd7b31
Emma Perlman <emmajperlman@gmail.com>
2022-05-23 20:07:59 -0700

    style cards for each time in teams list

--- src/components/player/TeamCard.js
+++ src/components/player/TeamCard.js
@@ -1,14 +1,56 @@
 import React from "react"
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 500,
+    justifyContent:'center'
+  },
+  media: {
+    justifyContent:'center',
+    height: "100px",
+    width: 'auto'
+  }
+});
+
 export const TeamCard = ({team,onShowRoster}) => {
   function onTeamClick(){
     onShowRoster(team)
   }
+  const classes = useStyles();
   return (
-    <div style={{margin:'auto'}} onClick={onTeamClick}>
-      <h2>{team.strTeam}</h2>
-      <h4>{team.strStadiumLocation}</h4>
-      <img src={team.strTeamBanner} alt={`${team.strTeam}`}/>
-      <p>{team.strDescriptionEN}</p>
-    </div>
+     <Card className={classes.root} onClick={onTeamClick}>
+     <CardActionArea>
+       <CardMedia
+         component="img"
+         className={classes.media}
+         image={team.strTeamBadge}
+         alt={`${team.strTeam}`}
+       />
+       <CardContent>
+         <Typography gutterBottom variant="h5" component="div">
+            {team.strTeam}
+         </Typography>
+         <Typography gutterBottom variant="h5" component="div">
+            {team.strStadiumLocation}
+         </Typography>
+         <Typography variant="body2" color="text.secondary">
+            {team.strDescriptionEN}
+         </Typography>
+       </CardContent>
+     </CardActionArea>
+     <CardActions>
+       <Button size="small" color="primary">
+         Share
+       </Button>
+     </CardActions>
+   </Card>
   )
 }
--- src/components/player/TeamList.js
+++ src/components/player/TeamList.js
@@ -1,10 +1,11 @@
+import { Grid } from "@mui/material"
 import React from "react"
 import {TeamCard} from './TeamCard'
 export const TeamList = ({teams,onShowRoster}) => {
   const teamCards=teams.map(team=> <TeamCard key={team.idTeam} team={team} onShowRoster={onShowRoster}/>)
   return (
-    <div>
+    <Grid>
       {teamCards}
-    </div>
+    </Grid>
   )
 }
